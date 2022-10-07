09407f2063eb04dc6857f8ab4800212b93ccc468
Bulavina Elena <koroleva488@mail.ru>
2022-10-07 11:06:00 +1000

    Задача 4

--- Task4.py
+++ Task4.py
@@ -1,5 +1,9 @@
 # Задача 4.	Реализуйте алгоритм перемешивания списка, без использования 
 # встроенных методов (особенно SHUFFLE, без него). Можно (нужно) использовать библиотеку Random.
 import random
-numbers=[1, 2, 3, 4, 5]
-print.random(numbers)
+list1=['1', 'Погода', '2', 'Good', '3', '***', '4', '5']
+print('Исходный список: ' + str(list1))
+for i in range (len(list1)-1):
+    j=random.randint(0, len(list1)-1)
+    list1[i], list1[j] = list1[j], list1[i]
+print('Перемешанный список: ' + str(list1))
